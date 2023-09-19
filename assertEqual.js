8af7974195e8fa5f99a3a1dd3fcfcfc4949baf4a
Neeharika Baral <neehabaral72@gmail.com>
2023-09-18 22:28:26 -0400

    completed assignment

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return "🎉🎉🎉 Assertion passed: " + actual + " === " + expected
+  } else {
+    return "❌❌❌ Assertion Failed: " + actual + " !== " + expected
+  }
+}
+
+console.log(assertEqual("Hello", "Hello")) // passed 
+console.log(assertEqual("Hi", "Hello")) // failed
+
+console.log(assertEqual(1, 1)) // passed
+console.log(assertEqual(1, 100)) // failed
+
+
