d8e8c898d4089240a33cdfb1d500ae9457dc127f
Neeharika Baral <neehabaral72@gmail.com>
2023-10-08 15:33:49 -0400

    initial commit

---
+++ flatten.js
@@ -0,0 +1,13 @@
+const flatten = function(array) {
+  let flatArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (const value of array[i]) {
+        flatArray.push(value);
+      }
+    } else {
+      flatArray.push(array[i]);
+    }
+  } return flatArray;
+};
+
