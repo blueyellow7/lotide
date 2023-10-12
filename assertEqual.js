eabf697579cda0cb2b21e1c04ae70b85def62a8e
Neeharika Baral <neehabaral72@gmail.com>
2023-10-12 12:33:12 -0400

    changed return to consolelog

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return `🎉🎉🎉 Assertion passed ${actual} === ${expected}`;
+    console.log(`🎉🎉🎉 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return `❌❌❌ Assertion failed ${actual} !== ${expected}`;
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
