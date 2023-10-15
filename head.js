d9943e3b6373fbc449d261db487d441e5bf3da79
Neeharika Baral <neehabaral72@gmail.com>
2023-10-15 19:56:42 -0400

    refractored and exported head function

--- head.js
+++ head.js
@@ -1,20 +1,6 @@
-// assertEqual function
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `🎉🎉🎉 Assertion passed ${actual} === ${expected}`;
-  } else {
-    return `❌❌❌ Assertion failed ${actual} !== ${expected}`;
-  }
-};
-
 const head = function(arrayValue) {
   return arrayValue[0];
-}
+};
 
-console.log(head([])) 
-console.log(head([1])) 
-console.log(assertEqual(head([5,6,7]), 5)) 
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")) 
-
-
+// export used by headTest file
+module.exports = head;
