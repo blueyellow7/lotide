c735b5605d4f9c981cacba0b3def21d0e844a754
Neeharika Baral <neehabaral72@gmail.com>
2023-10-15 19:57:42 -0400

    initial commit

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+
+// Test cases 
+
+assertEqual("Neeha", "Neeha");
+
+assertEqual("Neeha", "Lighthouse");
+
+assertEqual(72, 72);
+
+assertEqual(17, 86);
