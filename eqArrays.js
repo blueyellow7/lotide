a276c2d21cdaa73b168b029d3eaeacb05e78f06f
Neeharika Baral <neehabaral72@gmail.com>
2023-10-15 20:56:13 -0400

    refractored and exported

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `🎉🎉🎉 Assertion passed ${actual} === ${expected}`;
-  } else {
-    return `❌❌❌ Assertion failed ${actual} !== ${expected}`;
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
   for (i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
       return false;
     } 
   } return true;
 };
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true))
-console.log(assertEqual(eqArrays([1, 2], [1, 2, 3]), true))
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true))
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true))
+// export used by eqArraysTest file
+module.exports = eqArrays;
