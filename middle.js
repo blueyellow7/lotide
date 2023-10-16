0787212a990f5c50a50df6b4041019c1350ff3a9
Neeharika Baral <neehabaral72@gmail.com>
2023-10-15 21:30:40 -0400

    refractored and exported

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-//TESTING ASSERTION OF FUNCTIONS
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`🎉🎉🎉 Assertion passed ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌❌❌ Assertion failed ${arr1} !== ${arr2}`);
-  }
-};
-
-// ACTUAL FUNCTION 
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -31,8 +8,4 @@ const middle = function(array) {
   }
 };
 
-// TEST CODE
-
-assertArraysEqual(middle([5, 6, 7, 8, 9]), [7]);
-assertArraysEqual(middle([5, 6, 7, 8]), [6, 7]);
-assertArraysEqual(middle([5, 6]), []);
+module.exports = middle;
