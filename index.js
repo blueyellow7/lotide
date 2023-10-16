2ca3a85e6bd65c95d87ed281689293a6d9262c49
Neeharika Baral <neehabaral72@gmail.com>
2023-10-15 23:52:15 -0400

    Implemented Object Shorthand

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const head   = require('./head.js');
-const tail   = require('./tail.js');
+const head = require('./head.js');
+const tail = require('./tail.js');
 const middle = require('./middle.js');
 const assertArraysEqual = require('./assertArraysEquals.js');
 const assertEqual = require('./assertEqual.js');
@@ -7,7 +7,7 @@ const eqArrays = require('./eqArrays.js');
 const eqObjects = require('./eqObjects.js');
 const assertObjectsEqual = require('./assertObjectsEqual.js');
 const countLetters = require('./countLetters.js');
-const countOnly = require('./counttLetters.js');
+const countOnly = require('./counttOnly.js');
 const findKey = require('./findKey.js');
 const findKeyByValue = require('./findKeyByValue.js');
 const flatten = require('./flatten.js');
@@ -17,22 +17,22 @@ const takeUntil = require('./takeUntil.js');
 const without = require('./without.js');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  eqObjects,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
 
