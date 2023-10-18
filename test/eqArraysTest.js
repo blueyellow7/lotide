98df9d9862677b26635d40626037fb27e0fef81d
Neeharika Baral <neehabaral72@gmail.com>
2023-10-18 12:36:48 -0400

    implemented mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,18 +1,27 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
+describe("#eqArrays", () => {
+  it("should return true for [1, 2, 3, 4] and [1, 2, 3, 4]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true);
+  });
 
-// Test cases
+  it("should return false for [3, 2] and [3, 2, 1]", () => {
+    assert.strictEqual(eqArrays([3, 2], [3, 2, 1]), false);
+  });
 
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true)
+  it("should return true for ['5', '6', '7'] and ['5', '6', '7']", () => {
+    assert.strictEqual(eqArrays(['5', '6', '7'], ['5', '6', '7']), true);
+  });
 
-assertEqual(eqArrays([3, 2], [3, 2, 1]), false)
+  it("should return false for ['5', '6', '7'] and ['8', '9', '10']", () => {
+    assert.strictEqual(eqArrays(['5', '6', '7'], ['8', '9', '10']), false);
+  });
 
-assertEqual(eqArrays(["5", "6", "7", "8"], ["5", "6", "7", "8"]), true)
-
-assertEqual(eqArrays(["5", "6", "7"], ["7", "8", "9"]), false)
-
-assertEqual(eqArrays(["5", "6", "7"], ["5", "6", 7]), false)
+  it("should return false for ['5', '6', '7'] and ['5', '6', 7]", () => {
+    assert.strictEqual(eqArrays(['5', '6', '7'], ['5', '6', 7]), false);
+  });
+});
 
 
 
